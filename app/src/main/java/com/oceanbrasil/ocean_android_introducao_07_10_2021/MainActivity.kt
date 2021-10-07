ec90b1fbb9ee185d7b8209327af33f783411aa5a
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-07 15:50:19 -0300

    Atualização do texto ao clicar no botão

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_07_10_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_07_10_2021/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.oceanbrasil.ocean_android_introducao_07_10_2021
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Buscar o botão pelo ID
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        btEnviar.setOnClickListener {
+            tvResultado.text = "Olá, profs. Antonio Deusany e Paulo Salvatore!"
+        }
+
+        /*
+        Exercício:
+        Construir um elemento de EditText no layout (entre o TextView e o Botão)
+        Ao clicar no botão, pegar o que foi digitado no EditText e atualizar no texto do TextView
+        */
     }
 }
